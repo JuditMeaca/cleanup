5457b692d559f0783d4b3c8a9073e79de769c363
Judit Meaca <juditmeaca@gmail.com>
2020-12-03 12:57:23 -0300

    elimine archivos de testing

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,72 @@ import { map, tap } from 'rxjs/operators';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[] = [];
+  products: Product[] = [/*
+    {
+      category: 'Limpieza del Hogar',
+      name: 'Cera',
+      price: 120,
+      stock: 5,
+      image: 'assets/img/hogar.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    {
+      category: 'Limpieza del Hogar',
+      name: 'Detergente',
+      price: 90,
+      stock: 110,
+      image: 'assets/img/hogar.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    {
+      category: 'Limpieza del Hogar',
+      name: 'Lavalustre',
+      price: 156,
+      stock: 36,
+      image: 'assets/img/hogar.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    {
+      category: 'Jardines y Mascotas',
+      name: 'Veneno para plantas',
+      price: 89,
+      stock: 33,
+      image: 'assets/img/jardin.jpg',
+      sale: true,
+      quantity: 0,
+    },
+    {
+      category: 'Jardines y Mascotas',
+      name: 'Shampoo para perros',
+      price: 99,
+      stock: 44,
+      image: 'assets/img/jardin.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    {
+      category: 'Cosmetica Automotriz',
+      name: 'Shampoo para autos',
+      price: 200,
+      stock: 38,
+      image: 'assets/img/autos.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    {
+      category: 'Cosmetica Automotriz',
+      name: 'Revividor para gomas y vinílicos',
+      price: 220,
+      stock: 0,
+      image: 'assets/img/autos.jpg',
+      sale: false,
+      quantity: 0,
+    },
+    **/
+  ];
 
   constructor(
     private cart: ProductCartService,
